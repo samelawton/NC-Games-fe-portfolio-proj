a368241ac49b855e995a4fabc7fb73114df0ad54
samelawton <samuellawton18@gmail.com>
2023-03-13 16:26:58 +0000

    task 4 - reviews displayed

---
+++ src/Pages/ReviewsPage.jsx
@@ -0,0 +1,11 @@
+import ReviewsCard from "../components/ReviewsCard";
+
+const ReviewsPage = () => {
+    return ( 
+    <div>
+        <h2>Reviews</h2>
+        <ReviewsCard/>
+    </div> );
+}
+ 
+export default ReviewsPage;
---
+++ src/components/ReviewsCard.jsx
@@ -0,0 +1,40 @@
+import { useEffect, useState } from "react";
+import { fetchReviews } from "../utils/api";
+import ReviewBody from "./ReviewBody";
+
+const ReviewsCard = () => {
+
+    const [reviews, setReviews] = useState(null);
+    const [error, setError] = useState(null);
+    const [isLoading, setIsLoading] = useState(true); 
+    console.log(reviews)
+    useEffect(()=>{
+        setReviews([])
+        setIsLoading(true);
+        const promises = [
+            fetchReviews()
+        ];
+        Promise.all(promises)
+            .then((data)=>{
+                const reviewsData = data[0].data.reviews;
+                setReviews(reviewsData)
+                setError(null)
+            })
+            .catch((err)=>{
+                setError("Something went wrong. Could not fetch data.")
+            })
+            .finally(()=>{
+                setIsLoading(false)
+            })
+    }, [])
+
+    return (
+        <main className="reviews-card">
+            {reviews && <ReviewBody reviews={reviews}/>}
+            {isLoading && <p>Loading...</p>}
+            {error && <p>{error}</p>}
+        </main>
+     );
+}
+ 
+export default ReviewsCard;
