11923233dc90cc54d31989b2f6a6b0c9bb45cc1f
samelawton <samuellawton18@gmail.com>
2023-03-16 10:39:03 +0000

    comment posting ability added and code tidied up to be able to display page when comments are 0

--- src/components/CommentsItem.jsx
+++ src/components/CommentsItem.jsx
@@ -1,6 +1,18 @@
 import CommentBody from "./CommentBody";
 
 const CommentsItem = ({comments}) => {
+
+    // useEffect(() => {
+    //    fetchComments(review_id)
+    //       .then((data) => {
+    //         setComments(data[1].data.review);
+    //         setError(null);
+    //       })
+    //       .catch((err) => {
+    //         setError("Something went wrong. Could not fetch comments.");
+    //       });
+    //   }, [review_id]);
+
     return (
         <section>
         <ol key={comments.comment_id} className="comment-list">
--- src/components/ReviewItem.jsx
+++ src/components/ReviewItem.jsx
@@ -1,24 +1,40 @@
 import { Link } from "react-router-dom";
 
-const ReviewItem = ({review_id, owner, image, category, comment_count, created, designer, title, votes}) => {
-    
-    return ( 
-        <li className="review-list__item">
-            <article>
-            <img src={image} alt="game"/>
-            <div className="review-list__item__details">
-            <h3>{title}</h3>
-            <h4>User: {owner}</h4>
-            <p>Created at: {created}</p>
-            <p>Category: {category}</p>
-            <p>Designer: {designer}</p>
-            <p>Votes: {votes}</p>
-            <p>Comment count: {comment_count}</p>
-            <Link to={`/reviews/${review_id}`}><button value={review_id}>Open Review</button></Link>
-            </div>
-            </article>
-        </li>
-     );
-}
- 
+const ReviewItem = ({
+  review_id,
+  owner,
+  image,
+  category,
+  comment_count,
+  created,
+  designer,
+  title,
+  votes,
+}) => {
+  return (
+    <li className="review-list__item">
+      <article>
+        <img src={image} alt="game" />
+        <div className="review-list__item__details">
+          <section className="user-post-details">
+            <h4>Posted by {owner} </h4>
+            <p></p>
+            <h4> at {created}</h4>
+          </section>
+
+          <h3>{title}</h3>
+
+          <p>Category: {category}</p>
+          <p>Designer: {designer}</p>
+          <p>{votes} votes</p>
+          <p>{comment_count} Comments</p>
+          <Link to={`/reviews/${review_id}`}>
+            <button value={review_id}>Open Review</button>
+          </Link>
+        </div>
+      </article>
+    </li>
+  );
+};
+
 export default ReviewItem;
